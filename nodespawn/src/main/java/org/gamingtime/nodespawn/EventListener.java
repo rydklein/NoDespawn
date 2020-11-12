2cf8089c5e879c7e585293c3d9a3d188aa45f2b0
Ryder Klein <Ryder679@live.com>
2020-11-11 19:41:43 -0500

    Actually got it working. Very small oversight (huge oversight that completely broke everything.

--- nodespawn/src/main/java/org/gamingtime/nodespawn/EventListener.java
+++ nodespawn/src/main/java/org/gamingtime/nodespawn/EventListener.java
@@ -6,12 +6,12 @@ import java.util.List;
 import org.bukkit.event.Listener;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
-import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Item;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.event.entity.EntityDeathEvent;
-import de.tr7zw.changeme.nbtapi.NBTItem;
-
+import de.tr7zw.changeme.nbtapi.NBTEntity;
+import org.bukkit.World;
 public class EventListener implements Listener {
     private Main mainRef;
     private short idealAge = -32768;
@@ -21,20 +21,14 @@ public class EventListener implements Listener {
             return;
         }
         mainRef.getLogger().info("Player Died!");
-        List<ItemStack> newItems = new ArrayList<ItemStack>();
 
         event.getDrops().forEach(element -> {
-            NBTItem newDropNBT = new NBTItem(element);
-            mainRef.getLogger().info(newDropNBT.getShort("Age").toString());
+            Item droppedItem = event.getEntity().getWorld().dropItem(event.getEntity().getLocation(), element);
+            NBTEntity newDropNBT = new NBTEntity(droppedItem);
+            // mainRef.getLogger().info(newDropNBT.getShort("Age").toString());
             newDropNBT.setShort("Age", idealAge);
-            newItems.add(newDropNBT.getItem());
         });
         event.getDrops().clear();
-        event.getDrops().addAll(newItems);
-        event.getDrops().forEach(element -> {
-            NBTItem newDropNBT2 = new NBTItem(element);
-            mainRef.getLogger().info(newDropNBT2.getShort("Age").toString());
-        });
     }
 
     public void setMainRef(Main mainR) {
