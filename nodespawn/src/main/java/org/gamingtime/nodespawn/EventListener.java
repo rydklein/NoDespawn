5b3a5850880d9096dae8618d6e81f286a2ce62b2
Ryder Klein <Ryder679@live.com>
2020-11-11 21:55:15 -0500

    Added config, comments, and base for reloading config with command.

--- nodespawn/src/main/java/org/gamingtime/nodespawn/EventListener.java
+++ nodespawn/src/main/java/org/gamingtime/nodespawn/EventListener.java
@@ -1,37 +1,58 @@
 package org.gamingtime.nodespawn;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.bukkit.event.Listener;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.entity.Item;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.event.entity.EntityDeathEvent;
 import de.tr7zw.changeme.nbtapi.NBTEntity;
-import org.bukkit.World;
+
 public class EventListener implements Listener {
     private Main mainRef;
-    private short idealAge = -32768;
+    // If the Age NBT tag of the dropped items is set to this (minimum value), it
+    // never changes.
+    private short startingAge;
+    private Boolean disableDespawns;
+    private short noDespawnAge = -32768;
+
     @EventHandler(priority = EventPriority.HIGHEST)
     public void entityDeathEvent(EntityDeathEvent event) {
+        // Return if it's not a player who died.
         if (!(event.getEntity().getType().equals(org.bukkit.entity.EntityType.PLAYER))) {
             return;
         }
-        mainRef.getLogger().info("Player Died!");
-
+        // If inventory is empty, no need to waste power on all of this mess *gestures
+        // to code below*
+        if (event.getDrops().isEmpty()) {
+            return;
+        }
+        // For each intended drop item, spawn it in ourselves
         event.getDrops().forEach(element -> {
             Item droppedItem = event.getEntity().getWorld().dropItem(event.getEntity().getLocation(), element);
             NBTEntity newDropNBT = new NBTEntity(droppedItem);
-            // mainRef.getLogger().info(newDropNBT.getShort("Age").toString());
-            newDropNBT.setShort("Age", idealAge);
+            if (disableDespawns.equals(true)) {
+                newDropNBT.setShort("Age", noDespawnAge);
+            } else {
+                newDropNBT.setShort("Age", startingAge);
+            }
         });
         event.getDrops().clear();
     }
 
+    // Pass main class to event listener thru here.
     public void setMainRef(Main mainR) {
         mainRef = mainR;
+    }
+
+    public void setConfigs() {
+        // Loads config values from config.yml
+        int despawnTimeC = mainRef.getConfig().getInt("time-to-despawn");
+        disableDespawns = mainRef.getConfig().getBoolean("disable-despawns");
+        // If the values in our config are invalid, just default to normal behavior.
+        if ((despawnTimeC < 0) || (despawnTimeC > 38767)) {
+            startingAge = 6000;
+        } else {
+            startingAge = (short) (-1 * (despawnTimeC - 6000));
+        }
     }
 }
--- nodespawn/src/main/java/org/gamingtime/nodespawn/Main.java
+++ nodespawn/src/main/java/org/gamingtime/nodespawn/Main.java
@@ -1,15 +1,28 @@
 package org.gamingtime.nodespawn;
+
 import org.bukkit.plugin.java.JavaPlugin;
+
 public class Main extends JavaPlugin {
     public EventListener publicListener = new EventListener();
+
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(publicListener, this);
+
+        // I have no idea if this is the right way to access the main class from another
+        // class, but it works.
         publicListener.setMainRef(this);
+
+        // Tells EventHandler to load config options.
+        // It's in a function so that I can easily add a reload
+        // Config command later without having to refactor stuff.
+        publicListener.setConfigs();
+
+        // Put config there if it doesn't exist.
+        saveDefaultConfig();
         getLogger().info("NoDespawn Loaded!");
     }
-    @Override
-    public void onDisable() {
-        getLogger().info("NoDespawn Unloaded.");
-    }
+    /*
+     * @Override public void onDisable() { }
+     */
 }
